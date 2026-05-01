35031e750230c55dd6e865c3bdb3fd33172e493a
adailtonps <adailtonpereirasilveira9@gmail.com>
2026-04-30 21:41:43 -0300

    Deploy do sistema

--- backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/ContaRepository.java
+++ backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/ContaRepository.java
@@ -12,6 +12,5 @@ import java.util.Optional;
 public interface ContaRepository extends JpaRepository<Conta, Long> {
     boolean existsByClienteAndStatusConta(Cliente cliente, StatusConta statusConta);
     Optional<Conta> findByCliente(Cliente cliente);
-
 }
 
--- backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/TransacaoRepository.java
+++ backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/TransacaoRepository.java
@@ -2,12 +2,20 @@ package com.adps.sistemaBancario.repository;
 
 import com.adps.sistemaBancario.domain.Conta;
 import com.adps.sistemaBancario.domain.Transacao;
+import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
     List<Transacao> findByContaOrderByDataHoraTransacaoDesc(Conta conta);
-    void deleteByConta(Conta conta);
+    @Modifying
+    @Transactional
+    @Query("DELETE * FROM transacao t where t.conta = :conta")
+    void deleteByConta(@Param("conta")Conta conta);
 }
