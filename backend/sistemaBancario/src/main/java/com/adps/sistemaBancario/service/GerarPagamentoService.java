3a351e48f9d5bdd341876147f3a0d719ee47a364
adailton@ps.com <adailtonpereirasilveira9@gmail.com>
2026-08-08 16:16:46 -0300

    Pagamento testes

--- backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/service/GerarPagamentoService.java
+++ backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/service/GerarPagamentoService.java
@@ -16,21 +16,39 @@ import java.time.LocalDateTime;
 public class GerarPagamentoService {
     private PagamentoRepository pagamentoRepository;
 
-    public PagamentoResponseDTO criarPagamento(CriarPagamentoDTO criarPagamentoDTO){
+    public PagamentoResponseDTO criarPagamento(CriarPagamentoDTO criarPagamentoDTO) {
+
+        System.out.println(">>> 1 - ENTROU NO SERVICE");
+
         Pagamento pagamento = new Pagamento();
-        System.out.println(">>> PASSOU PELO SERVICE");
-        LocalDateTime dataHoraPagamento =  LocalDateTime.now();
+
+        LocalDateTime dataHoraPagamento = LocalDateTime.now();
+
         pagamento.setIdPedido(criarPagamentoDTO.getIdPedido());
         pagamento.setValorTotal(criarPagamentoDTO.getValorTotal());
+
+        System.out.println(">>> 2 - DADOS DO PAGAMENTO DEFINIDOS");
+
         pagamento.setStatusPagamento(StatusPagamento.PENDENTE_PAGAMENTO);
         pagamento.setDataCriacao(dataHoraPagamento);
         pagamento.setDataExpiracao(dataHoraPagamento.plusMinutes(30));
+
+        System.out.println(">>> 3 - ANTES DE GERAR CÓDIGO");
+
         pagamento.gerarCodigoPagamento();
 
+        System.out.println(">>> 4 - CÓDIGO GERADO");
+
         pagamentoRepository.save(pagamento);
+
+        System.out.println(">>> 5 - PAGAMENTO SALVO");
+
         PagamentoResponseDTO response = new PagamentoResponseDTO();
 
         response.setCodigoPagamento(pagamento.getCodigoPagamento());
+
+        System.out.println(">>> 6 - RESPONSE CRIADO");
+
         return response;
     }
 }
