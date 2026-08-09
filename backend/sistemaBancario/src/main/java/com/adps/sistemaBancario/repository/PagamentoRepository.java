89088c449cebcf415416525223a52eb47ae0c57c
adailton@ps.com <adailtonpereirasilveira9@gmail.com>
2026-08-08 21:14:07 -0300

    Pagamento testes

--- backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/PagamentoRepository.java
+++ backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/repository/PagamentoRepository.java
@@ -2,9 +2,11 @@ package com.adps.sistemaBancario.repository;
 
 import com.adps.sistemaBancario.domain.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PagamentoRepository extends JpaRepository<Pagamento, String> {
     Optional<Pagamento> findByCodigoPagamento(String codigoPagamento);
 }
