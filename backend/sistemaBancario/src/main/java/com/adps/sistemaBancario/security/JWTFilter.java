485509bf77e07ac26b471f56e4ca14226d9cca3a
adailton@ps.com <adailtonpereirasilveira9@gmail.com>
2026-08-10 01:21:20 -0300

    Consertando bug da funcionalidade pagamento

--- backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/security/JWTFilter.java
+++ backend/sistemaBancario/src/main/java/com/adps/sistemaBancario/security/JWTFilter.java
@@ -33,26 +33,34 @@ public class JWTFilter extends OncePerRequestFilter {
         throws ServletException, IOException{
 
         SecurityContextHolder.clearContext();
+        System.out.println("JWT FILTER EXECUTOU");
+
 
         String header = request.getHeader("Authorization");
 
+        System.out.println("HEADER: " + header);
         if(header != null && header.startsWith("Bearer ")){
             String token = header.replace("Bearer ", "");
 
+            System.out.println("TOKEN ENCONTRADO");
             String email = jwtService.validarToken(token);
 
+            System.out.println("EMAIL DO TOKEN: " + email);
             if (email != null){
                 Cliente cliente = clienteRepository.findByEmail(email).orElse(null);
+
+                System.out.println("CLIENTE ENCONTRADO: " + cliente);
 
                 if(cliente != null){
                     UsernamePasswordAuthenticationToken auth=
                             new UsernamePasswordAuthenticationToken(
                                     cliente,
                                     null,
-                                    List.of(() -> "ROLE_CLIENTE")
+                                    List.of()
                             );
                           SecurityContextHolder.getContext().setAuthentication(auth);
 
+                    System.out.println("AUTH CRIADA: " + auth);
                 }
             }
         }
